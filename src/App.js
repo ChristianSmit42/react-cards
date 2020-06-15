c73eee54e4b48e01d562abc0e33e0b1967d32407
Christian Smit <chrsmit@web.de>
2020-06-15 17:04:49 +0200

    Add Card component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from "./components/Header";
+import MainContent from "./components/MainContent";
 
 export default function App() {
   return (
     <div className="App">
       <Header> Cards Collection </Header>
+        <MainContent title={"Animal Collection"}></MainContent>
     </div>
   );
 }
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,9 +1,21 @@
 import "./MainContent.css";
 
+import Card from "./Card";
+
 import React from "react";
 
 export default function MainContent(props) {
     return(
-
+        <main>
+            <button>button 1</button><button>button 2</button>
+            <div className={"Cards-Collection"}>
+                <Card title={"Ersatztitel"} imgsource={"https://i.pinimg.com/originals/59/30/c8/5930c86254cf1dc8064a63cc4c11f714.jpg"}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, illo.
+                </Card>
+                <Card title={"Ersatztitel"} imgsource={"https://i.pinimg.com/originals/59/30/c8/5930c86254cf1dc8064a63cc4c11f714.jpg"}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, illo.
+                </Card>
+            </div>
+        </main>
     );
 }
