d17d19b9aeb14f45ccc9c2fdb3b86a38eafaccfb
Christian Smit <chrsmit@web.de>
2020-06-15 17:39:39 +0200

    Update MainContent component to use array of Animals to create animal amount of cards

--- src/App.js
+++ src/App.js
@@ -2,12 +2,37 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
 
 export default function App() {
+    const animals = [
+        {
+            title: "Giraffe",
+            text: "vhfsvjhfdbsgdh oisfbjhsdb djfbg ksdbiu v  dfgpisfpisdg ",
+            imgsource:"https://i.pinimg.com/originals/59/30/c8/5930c86254cf1dc8064a63cc4c11f714.jpg",
+        },
+        {
+            title: "Affe",
+            text: "vhfsvjhfdbsgdh oisfbjhsdb djfbg ksdbiu v  dfgpisfpisdg ",
+            imgsource:"https://i.pinimg.com/originals/59/30/c8/5930c86254cf1dc8064a63cc4c11f714.jpg",
+        },
+        {
+            title: "Biene",
+            text: "vhfsvjhfdbsgdh oisfbjhsdb djfbg ksdbiu v  dfgpisfpisdg ",
+            imgsource:"https://i.pinimg.com/originals/59/30/c8/5930c86254cf1dc8064a63cc4c11f714.jpg",
+        },
+        {
+            title: "Biene",
+            text: "vhfsvjhfdbsgdh oisfbjhsdb djfbg ksdbiu v  dfgpisfpisdg ",
+            imgsource:"https://i.pinimg.com/originals/59/30/c8/5930c86254cf1dc8064a63cc4c11f714.jpg",
+        },
+    ]
+
   return (
     <div className="App">
-      <Header> Cards Collection </Header>
-        <MainContent title={"Animal Collection"}></MainContent>
+        <Header> Cards Collection </Header>
+        <MainContent animals={animals}></MainContent>
+        <Footer>footer Text</Footer>
     </div>
   );
 }
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -9,12 +9,13 @@ export default function MainContent(props) {
         <main>
             <button>button 1</button><button>button 2</button>
             <div className={"Cards-Collection"}>
-                <Card title={"Ersatztitel"} imgsource={"https://i.pinimg.com/originals/59/30/c8/5930c86254cf1dc8064a63cc4c11f714.jpg"}>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, illo.
-                </Card>
-                <Card title={"Ersatztitel"} imgsource={"https://i.pinimg.com/originals/59/30/c8/5930c86254cf1dc8064a63cc4c11f714.jpg"}>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, illo.
-                </Card>
+                {
+                    props.animals.map(
+                        (animal)=> {
+                            return <Card title={animal.title} imgsource={animal.imgsource}>{animal.text}</Card>;
+                        }
+                    )
+                }
             </div>
         </main>
     );
