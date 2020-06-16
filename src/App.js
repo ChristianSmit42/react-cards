eec8d3d7edcbd4aca3d7d31f115676f8d5d040b2
Christian Smit <chrsmit@web.de>
2020-06-16 14:21:03 +0200

    Update code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
-import Card from "./components/Card";
 import Navigation from "./components/Navigation";
 
 export default function App() {
